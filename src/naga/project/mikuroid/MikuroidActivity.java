014569f89db6df9353bc9495dc09497018bf1ef6
reciente <satotatsuma@gmail.com>
2010-12-22 01:46:49 +0900

    Create project.

---
+++ src/naga/project/mikuroid/MikuroidActivity.java
@@ -0,0 +1,13 @@
+package naga.project.mikuroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MikuroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
