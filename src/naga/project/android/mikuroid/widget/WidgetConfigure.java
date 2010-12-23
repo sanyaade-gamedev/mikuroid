6b6184b01471da235b69ef670c2c4b16de672d8b
reciente <reciente@localhost>
2010-12-24 01:15:51 +0900

    Cut off XML validator. Use android SDK validator.

--- src/naga/project/android/mikuroid/widget/WidgetConfigure.java
+++ src/naga/project/android/mikuroid/widget/WidgetConfigure.java
@@ -4,24 +4,30 @@ import naga.project.android.mikuroid.R;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * The configuration screen for the WidgetProvider widget.
+ * 
+ * @author reciente
+ * 
+ */
 public class WidgetConfigure extends Activity {
 
-	static final String TAG = "MikuroidWidgetConfigure";
+    private static final String TAG = "WidgetConfigure";
 
-	public WidgetConfigure() {
-		super();
-	}
+    public WidgetConfigure() {
+        super();
+    }
 
-	@Override
-	public void onCreate(Bundle icicle) {
-		super.onCreate(icicle);
-		
-		// Set the result to CANCELED. This will cause the widget host to cancel
-		// out of the widget placement if they press the back button.
-		setResult(Activity.RESULT_CANCELED);
-		
-		// Bind the action for the save button.
-		this.findViewById(R.id.widget);
-	}
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        // Set the result to CANCELED. This will cause the widget host to cancel
+        // out of the widget placement if they press the back button.
+        setResult(Activity.RESULT_CANCELED);
+
+        // Bind the action for the save button.
+        this.findViewById(R.id.widget);
+    }
 
 }
