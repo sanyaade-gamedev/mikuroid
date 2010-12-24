b7fbd284bce00aa7ebb31643681e2325fb090f3e
reciente <reciente@localhost>
2010-12-25 01:17:51 +0900

    Create miku message editor.

--- src/naga/project/android/mikuroid/widget/WidgetCharacter.java
+++ src/naga/project/android/mikuroid/widget/WidgetCharacter.java
@@ -2,6 +2,7 @@ package naga.project.android.mikuroid.widget;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.view.View;
 import android.widget.EditText;
 
 /**
@@ -11,16 +12,27 @@ import android.widget.EditText;
  *
  */
 public class WidgetCharacter {
+
+    WidgetCharacter(String name) {
+        this.prefName = name;
+    }
     
-    public void saveMessage(Context context, int appWidgetId, String text) {
+    public void create(EditText et) {
+        this.editText = et;
+        
+        this.editText.setText("はじめましてっ！みくだよっ！！");
+    }
+
+    public void click(View v, Context context, int appWidgetId) {
+        //String message = this.editText.getText().toString();
+        this.saveMessage(context, appWidgetId, "みくだよっ！");
+    }
+
+    private void saveMessage(Context context, int appWidgetId, String text) {
         SharedPreferences.Editor prefs =
             context.getSharedPreferences(this.prefName, Context.MODE_PRIVATE).edit();
         prefs.putString(this.prefName + appWidgetId, text);
         prefs.commit();
-    }
-
-    WidgetCharacter(String name) {
-        this.prefName = name;
     }
 
     private String prefName;
--- src/naga/project/android/mikuroid/widget/WidgetConfigure.java
+++ src/naga/project/android/mikuroid/widget/WidgetConfigure.java
@@ -2,8 +2,11 @@ package naga.project.android.mikuroid.widget;
 
 import naga.project.android.mikuroid.R;
 import android.app.Activity;
+import android.appwidget.AppWidgetManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 /**
  * The configuration screen for the WidgetProvider widget.
@@ -17,6 +20,8 @@ public class WidgetConfigure extends Activity {
 
     public WidgetConfigure() {
         super();
+        
+        this.miku = new WidgetCharacter("naga.project.android.WidgetCharacter.MikuHatsune");
     }
 
     @Override
@@ -31,6 +36,21 @@ public class WidgetConfigure extends Activity {
 
         // Bind the action for the save button.
         this.findViewById(R.id.widget);
+        
+        this.miku.create((EditText)this.findViewById(R.id.miku_edittext));
     }
+    
+    View.OnClickListener mikuClickListener = new View.OnClickListener() {
+
+        public void onClick(View v) {
+            // Miku has clicked.
+            // Use AppWidgetManager.INVALID_APPWIDGET_ID to share edit message with any widgets.
+            miku.click(v, WidgetConfigure.this, AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+    };
+    
+    /** Widget character Miku Hatsune. */
+    private WidgetCharacter miku;
 
 }
