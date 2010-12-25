af2d8abca3a647ca9b134b1c26602dc163b4f920
reciente <reciente@localhost>
2010-12-25 20:14:38 +0900

    Clean up source code.

--- src/naga/project/android/mikuroid/widget/WidgetService.java
+++ src/naga/project/android/mikuroid/widget/WidgetService.java
@@ -1,12 +1,9 @@
 package naga.project.android.mikuroid.widget;
 
 import android.app.Service;
-import android.appwidget.AppWidgetManager;
-import android.content.ComponentName;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
-import android.widget.RemoteViews;
 
 public class WidgetService extends Service {
 
@@ -16,25 +13,14 @@ public class WidgetService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(WidgetService.TAG, "onCreate()");
+        WidgetManager.getInstance().setContext(this);
     }
 
     @Override
     public void onStart(Intent intent, int startId) {
         super.onStart(intent, startId);
         Log.d(WidgetService.TAG, "onStart()");
-        
-        WidgetManager.getInstance().setContext(this);
 
-        // Build the widget update
-        RemoteViews updateViews = WidgetManager.getInstance().buildUpdate(this);
-        Log.d(WidgetService.TAG, "update built");
-
-        // Push update for this widget to the home screen
-        ComponentName thiswiget = new ComponentName(this, WidgetProvider.class);
-        AppWidgetManager manager = AppWidgetManager.getInstance(this);
-        manager.updateAppWidget(thiswiget, updateViews);
-        Log.d(WidgetService.TAG, "widget updated");
-        
         WidgetManager.getInstance().updateRemoteViews();
     }
 
