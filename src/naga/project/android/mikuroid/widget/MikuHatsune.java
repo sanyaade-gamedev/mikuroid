9afd846e4aec21a163d2ff824ca68c257fe1b384
reciente <reciente@localhost>
2010-12-30 23:45:20 +0900

    Success to show image from network.

--- src/naga/project/android/mikuroid/widget/MikuHatsune.java
+++ src/naga/project/android/mikuroid/widget/MikuHatsune.java
@@ -1,9 +1,11 @@
 package naga.project.android.mikuroid.widget;
 
+import android.graphics.Bitmap;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.RemoteViews;
 import naga.project.android.mikuroid.R;
+import naga.project.android.network.NetworkManager;
 import naga.project.android.network.NetworkNicovideo;
 
 public class MikuHatsune extends WidgetCharacter {
@@ -13,6 +15,8 @@ public class MikuHatsune extends WidgetCharacter {
   private static final int messageId = R.id.miku_message;
 
   private static final int balloonId = R.id.baloon0;
+
+  private static final int nicovideoImageId = R.id.nicovideo_image;
 
   public MikuHatsune() {
     super();
@@ -30,6 +34,14 @@ public class MikuHatsune extends WidgetCharacter {
   public void view(RemoteViews views) {
     if (this.message.length() == 0) {
       views.setViewVisibility(MikuHatsune.balloonId, ImageView.INVISIBLE);
+
+      Bitmap bitmap = NetworkManager.getInstance().load(
+          "http://tn-skr2.smilevideo.jp/smile?i=13136668");
+
+      if (null != bitmap) {
+        views.setViewVisibility(MikuHatsune.nicovideoImageId, ImageView.VISIBLE);
+        views.setImageViewBitmap(MikuHatsune.nicovideoImageId, bitmap);
+      }
     } else {
       views.setViewVisibility(MikuHatsune.balloonId, ImageView.VISIBLE);
       views.setTextViewText(MikuHatsune.messageId, this.message.toString());
