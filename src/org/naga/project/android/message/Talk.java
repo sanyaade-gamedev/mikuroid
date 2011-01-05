822e12062f451fd6d78b56e90ad49be0813d8513
reciente <reciente@localhost>
2011-01-06 00:23:36 +0900

    Merge NagaEngineAndroid classes. Because MercurialEclipse has a problem using with android library.

---
+++ src/org/naga/project/android/message/Talk.java
@@ -0,0 +1,162 @@
+package org.naga.project.android.message;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import android.os.Handler;
+import android.os.Message;
+
+public class Talk {
+
+  public Talk(TalkView tv, long tspeed) {
+    this.talkView = tv;
+    this.talkSpeed = tspeed;
+
+    this.messageQueue = new ConcurrentLinkedQueue<String>();
+    this.message = new StringBuilder();
+    this.talking = false;
+    this.initTalk();
+  }
+
+  private TalkView talkView;
+
+  public void Destroy() {
+    this.message.setLength(0);
+    this.messageQueue.clear();
+    this.talking = false;
+    this.forceStop();
+  }
+
+  public static final int TALK_MESSAGE = 1;
+  public static final int SHOW_MESSAGE = 2;
+  public static final int TALK_STOP = 3;
+
+  private Handler talkHandler = new Handler() {
+
+    @Override
+    public void handleMessage(Message msg) {
+      switch (msg.what) {
+      case Talk.TALK_MESSAGE:
+        processTalking();
+        // View talking message.
+        talkView.view();
+        break;
+
+      case Talk.SHOW_MESSAGE:
+        // Stop taling and show all.
+        showAll();
+        break;
+
+      case Talk.TALK_STOP:
+        // Stop talking.
+        forceStop();
+
+        break;
+      }
+    }
+
+  };
+
+  private void initTalk() {
+    this.messageIndex = 0;
+    this.currentMessage = "";
+    this.message.setLength(0);
+  }
+
+  private void processTalking() {
+    if (this.messageIndex >= this.currentMessage.length()) {
+      this.talkHandler.sendEmptyMessage(Talk.TALK_STOP);
+      return;
+    }
+    // Return line.
+    if (this.messageIndex % 12 == 0) {
+      this.message.append("\n");
+    }
+    this.message.append(this.currentMessage.charAt(this.messageIndex));
+    this.messageIndex++;
+
+    this.talkHandler.sendEmptyMessageDelayed(Talk.TALK_MESSAGE, talkSpeed);
+  }
+
+  /**
+   * Process talk.
+   */
+  public void process() {
+    // Finish speaking and show all message.
+    if (this.talking) {
+      this.talkHandler.sendEmptyMessage(Talk.SHOW_MESSAGE);
+      return;
+    }
+
+    this.initTalk();
+
+    this.currentMessage = this.messageQueue.poll();
+    if (null == this.currentMessage) {
+      this.currentMessage = "";
+      return;
+    }
+
+    this.talking = true;
+
+    this.talkHandler.sendEmptyMessage(Talk.TALK_MESSAGE);
+  }
+
+  /**
+   * Stop taling and show message.
+   */
+  public void showAll() {
+    synchronized (talking) {
+      talking = false;
+    }
+    
+    this.messageIndex = 0;
+    
+  }
+
+  /**
+   * Stop taling and clear data.
+   */
+  private void forceStop() {
+    synchronized (talking) {
+      talking = false;
+    }
+    this.talkHandler.removeMessages(Talk.TALK_MESSAGE);
+    this.talkHandler.removeMessages(Talk.TALK_STOP);
+    this.messageQueue.clear();
+  }
+
+  private ConcurrentLinkedQueue<String> messageQueue;
+
+  public ConcurrentLinkedQueue<String> getMessageQueue() {
+    return messageQueue;
+  }
+
+  /**
+   * Message to show display.
+   */
+  private StringBuilder message;
+
+  /**
+   * True when character is talking.
+   */
+  private Boolean talking;
+
+  /**
+   * Use to talk. Index of currentMessage. To show message step by step.
+   */
+  private int messageIndex;
+
+  /**
+   * Current message that character is talking.
+   */
+  private String currentMessage;
+
+  /**
+   * Talking speed. milliseconds
+   */
+  private long talkSpeed;
+
+  public StringBuilder getMessage() {
+    return message;
+  }
+
+}
---
+++ src/org/naga/project/nicovideo/NicovideoLoader.java
@@ -0,0 +1,100 @@
+package org.naga.project.nicovideo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import android.util.Xml;
+
+abstract public class NicovideoLoader {
+
+  private static final String ENCODING = "UTF-8";
+
+  private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss+09:00";
+
+  public static List<NicovideoEntry> LoadEntryFromFeed(InputStream is) {
+    XmlPullParser parser = Xml.newPullParser();
+    try {
+      parser.setInput(is, NicovideoLoader.ENCODING);
+    } catch (XmlPullParserException e) {
+      e.printStackTrace();
+      return null;
+    }
+
+    List<NicovideoEntry> entryList = new ArrayList<NicovideoEntry>();
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+        NicovideoLoader.DATE_FORMAT);
+
+    try {
+      int eventType = parser.getEventType();
+      NicovideoEntry entry = null;
+
+      while (eventType != XmlPullParser.END_DOCUMENT) {
+        String name = null;
+
+        if (eventType == XmlPullParser.START_TAG) {
+          name = parser.getName();
+
+          if ("entry".equals(name)) {
+            // New entry. <entry>
+            entry = new NicovideoEntry();
+          }
+
+          if (null != entry) {
+            if ("title".equals(name)) {
+              entry.setTitle(parser.nextText());
+            } else if ("link".equals(name)) {
+              entry.setLink(parser.getAttributeValue(null, "href"));
+            } else if ("id".equals(name)) {
+              entry.setId(parser.nextText());
+            } else if ("published".equals(name)) {
+              Date date = null;
+              try {
+                date = simpleDateFormat.parse(parser.nextText());
+              } catch (ParseException e) {
+                e.printStackTrace();
+                date = null;
+              }
+              entry.setPublished(date);
+            } else if ("updated".equals(name)) {
+              Date date = null;
+              try {
+                date = simpleDateFormat.parse(parser.nextText());
+              } catch (ParseException e) {
+                e.printStackTrace();
+                date = null;
+              }
+              entry.setUpdated(date);
+            } else if ("content".equals(name)) {
+              entry.setContent(parser.nextText());
+            }
+          }
+        } else if (eventType == XmlPullParser.END_TAG) {
+          name = parser.getName();
+          if ("entry".equals(name)) {
+            // Find </entry>
+            if (null != entry) {
+              entryList.add(entry);
+            }
+          }
+        }
+
+        eventType = parser.next();
+      }
+    } catch (XmlPullParserException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return entryList;
+  }
+
+}
