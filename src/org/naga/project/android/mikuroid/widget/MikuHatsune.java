2109710d3da74f7f483b5aafd91d2e09afecadc1
reciente <reciente@localhost>
2011-01-05 22:57:23 +0900

    Use talking library from NagaEngineAndroid.

--- src/org/naga/project/android/mikuroid/widget/MikuHatsune.java
+++ src/org/naga/project/android/mikuroid/widget/MikuHatsune.java
@@ -9,64 +9,60 @@ import android.graphics.Bitmap;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.RemoteViews;
+
+import org.naga.project.android.message.Talk;
 import org.naga.project.android.mikuroid.R;
 import org.naga.project.nicovideo.NicovideoEntry;
 import org.naga.project.nicovideo.NicovideoNetwork;
 import org.naga.project.nicovideo.NicovideoUtil;
 
-public class MikuHatsune extends WidgetCharacter {
+public class MikuHatsune {
 
   public MikuHatsune() {
     super();
-    Log.d("MikuHatsune", "constructor");
 
-    this.messageQueue.add("みっくみっくにしてあげる～♪");
+    Log.d("MikuHatsune", "constructor");
+  }
+
+  public boolean create() {
+    this.talk = new Talk(WidgetManager.getInstance(), 100);
+    this.talk.getMessageQueue().add("みっくみっくにしてあげる～♪");
 
     List<NicovideoEntry> entryList = NicovideoNetwork
         .requestDailyRankingVOCALOID();
-    this.nicoEntryQueue.addAll(entryList);
+    // this.nicoEntryQueue.addAll(entryList);
 
     new DownloadImageTask().execute(NicovideoUtil
         .generateThumbnailUrls(entryList));
+
+    return true;
   }
 
   public void update() {
-    // Finish speaking and show all message.
-    if (this.talking) {
-      this.talkHandler.sendEmptyMessage(WidgetCharacter.TALK_STOP);
-      return;
-    }
-
-    this.initTalk();
-
-    this.currentMessage = this.messageQueue.poll();
-    if (null == this.currentMessage) {
-      this.currentMessage = "";
-      return;
-    }
-
-    this.talking = true;
-
-    this.talkHandler.sendEmptyMessage(WidgetCharacter.TALK_MESSAGE);
+    this.talk.process();
   }
 
   public void view(RemoteViews views) {
-    if (this.message.length() == 0) {
-      views.setViewVisibility(R.id.baloon0, ImageView.INVISIBLE);
-
-      Bitmap bitmap = NetworkManager.getInstance().load(
-          "http://tn-skr2.smilevideo.jp/smile?i=13136668");
-
-      if (null != bitmap) {
-        views.setViewVisibility(R.id.nicovideo_image, ImageView.VISIBLE);
-        views.setImageViewBitmap(R.id.nicovideo_image, bitmap);
-      }
+    if (this.talk.getMessage().length() == 0) {
+      /*
+       * views.setViewVisibility(R.id.baloon0, ImageView.INVISIBLE);
+       * 
+       * Bitmap bitmap = NetworkManager.getInstance().load(
+       * "http://tn-skr2.smilevideo.jp/smile?i=13136668");
+       * 
+       * if (null != bitmap) { views.setViewVisibility(R.id.nicovideo_image,
+       * ImageView.VISIBLE); views.setImageViewBitmap(R.id.nicovideo_image,
+       * bitmap); }
+       */
 
     } else {
       views.setViewVisibility(R.id.nicovideo_image, ImageView.INVISIBLE);
       views.setViewVisibility(R.id.baloon0, ImageView.VISIBLE);
-      views.setTextViewText(R.id.miku_message, this.message.toString());
+      views.setTextViewText(R.id.miku_message, this.talk.getMessage()
+          .toString());
     }
   }
+
+  private Talk talk;
 
 }
