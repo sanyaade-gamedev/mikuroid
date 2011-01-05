8d32b68740ea95f117ddbcd4a276b1a51928f0fb
reciente <reciente@localhost>
2011-01-05 21:03:17 +0900

    Add getter and setter.

--- src/org/naga/project/android/nicovideo/NicovideoContent.java
+++ src/org/naga/project/android/nicovideo/NicovideoContent.java
@@ -6,4 +6,20 @@ public class NicovideoContent {
 
   private String description;
 
+  public String getImgSrc() {
+    return imgSrc;
+  }
+
+  public void setImgSrc(String imgSrc) {
+    this.imgSrc = imgSrc;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
 }
