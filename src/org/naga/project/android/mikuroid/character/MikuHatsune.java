837adcc7f368e60f9b138c064af08c468c30e7d7
reciente <reciente@localhost>
2011-01-08 07:32:01 +0900

    Controll WidgetMode by WidgetManger.

--- src/org/naga/project/android/mikuroid/character/MikuHatsune.java
+++ src/org/naga/project/android/mikuroid/character/MikuHatsune.java
@@ -23,8 +23,6 @@ public class MikuHatsune {
   public boolean create() {
     Log.d("MikuHatsune", "create()");
 
-    this.mode = WidgetObject.WidgetMode.TALK;
-
     this.talk = new Talk(WidgetManager.getInstance(), 100, 20);
 
     Resources res = WidgetManager.getInstance().getContext().getResources();
@@ -42,70 +40,41 @@ public class MikuHatsune {
     return true;
   }
 
-  public void update() {
-    switch (this.mode) {
-    case WAIT:
-      this.waitUpdate();
-      break;
+  /*
+   * Bitmap bitmap = NetworkManager.getInstance().load(
+   * "http://tn-skr2.smilevideo.jp/smile?i=13136668");
+   * 
+   * if (null != bitmap) { views.setViewVisibility(R.id.nicovideo_image,
+   * ImageView.VISIBLE); views.setImageViewBitmap(R.id.nicovideo_image, bitmap);
+   * }
+   */
 
-    case TALK:
-      this.talkUpdate();
-      break;
-
-    case NICOVIDEO_RANKING:
-      // TODO Update nicovideo ranking process.
-      break;
-    }
-  }
-
-  public void view(RemoteViews views) {
-    switch (this.mode) {
-    case WAIT:
-      this.waitView(views);
-
-    case TALK:
-      this.talkView(views);
-      break;
-
-    case NICOVIDEO_RANKING:
-      // TODO View nicovideo ranking.
-      break;
-    }
-
-    /*
-     * Bitmap bitmap = NetworkManager.getInstance().load(
-     * "http://tn-skr2.smilevideo.jp/smile?i=13136668");
-     * 
-     * if (null != bitmap) { views.setViewVisibility(R.id.nicovideo_image,
-     * ImageView.VISIBLE); views.setImageViewBitmap(R.id.nicovideo_image,
-     * bitmap); }
-     */
-  }
-
-  private void waitUpdate() {
+  public void waitUpdate() {
     // Add talk message.
     String message = MikuMessage.generateBatteryMessage();
     if (null != message) {
       // Finish to talk.
-      this.mode = WidgetObject.WidgetMode.TALK;
+      // Change widget mode immediately.
+      WidgetManager.getInstance().setMode(WidgetObject.WidgetMode.TALK);
       this.talk.getMessageQueue().add(message);
       this.talk.process();
     }
   }
 
-  private void waitView(RemoteViews views) {
+  public void waitView(RemoteViews views) {
     views.setViewVisibility(R.id.baloon0, ImageView.INVISIBLE);
     views.setImageViewResource(R.id.miku, MikuHatsune.SURFACE_SURPRISED);
   }
 
-  private void talkUpdate() {
+  public void talkUpdate() {
     if (!this.talk.process()) {
       // Nothing to talk.
-      this.mode = WidgetObject.WidgetMode.WAIT;
+      // Change widget mode immediately.
+      WidgetManager.getInstance().setMode(WidgetObject.WidgetMode.WAIT);
     }
   }
 
-  private void talkView(RemoteViews views) {
+  public void talkView(RemoteViews views) {
     if (this.talk.getMessage().length() == 0) {
       views.setViewVisibility(R.id.baloon0, ImageView.INVISIBLE);
     } else {
@@ -117,8 +86,6 @@ public class MikuHatsune {
           .toString());
     }
   }
-
-  private WidgetObject.WidgetMode mode;
 
   private Talk talk;
 
