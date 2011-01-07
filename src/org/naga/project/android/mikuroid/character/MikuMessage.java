aff60d3a990ed1c72b04977b98b52d28a8fcfe43
reciente <reciente@localhost>
2011-01-08 05:51:24 +0900

    Add message for battery is full.

--- src/org/naga/project/android/mikuroid/character/MikuMessage.java
+++ src/org/naga/project/android/mikuroid/character/MikuMessage.java
@@ -5,16 +5,20 @@ import org.naga.project.android.mikuroid.widget.WidgetManager;
 
 public abstract class MikuMessage {
 
-  private static final int BATTERY_LOW_LEVL = 30;
+  private static final int BATTERY_LOW_LEVEL = 30;
+  private static final int BATTERY_FULL_LEVEL = 100;
 
   public static String generateBatteryMessage() {
     int batteryLevel = WidgetManager.getInstance().getCurrentBatteryLevel();
 
     String message = null;
 
-    if (batteryLevel < MikuMessage.BATTERY_LOW_LEVL) {
+    if (batteryLevel < MikuMessage.BATTERY_LOW_LEVEL) {
       message = WidgetManager.getInstance().getContext().getResources()
           .getString(R.string.battery_low);
+    } else if (batteryLevel == MikuMessage.BATTERY_FULL_LEVEL) {
+      message = WidgetManager.getInstance().getContext().getResources()
+          .getString(R.string.battery_full);
     } else {
       message = WidgetManager.getInstance().getContext().getResources()
           .getString(R.string.battery_good);
