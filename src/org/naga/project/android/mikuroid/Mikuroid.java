15a31c0e767bce684ab135430aa52de6ce618caf
reciente <reciente@localhost>
2011-01-08 22:27:11 +0900

    Get PendingIntent when clicked yes no button.

---
+++ src/org/naga/project/android/mikuroid/Mikuroid.java
@@ -0,0 +1,11 @@
+package org.naga.project.android.mikuroid;
+
+public class Mikuroid {
+
+  public static final String ACTION_MIKU_TOUCH = "org.naga.project.android.mikuroid.ACTION_MIKU_TOUCH";
+
+  public static final String ACTION_YES = "org.naga.project.android.mikuroid.ACTION_YES";
+
+  public static final String ACTION_NO = "org.naga.project.android.mikuroid.ACTION_NO";
+
+}
