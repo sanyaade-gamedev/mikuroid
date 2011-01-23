5c231a4af5e019b164798cd7e8cd163e3b12951a
reciente <reciente@localhost>
2011-01-24 05:34:55 +0900

    Redesigned classes.

---
+++ src/org/naga/project/android/action/Action.java
@@ -0,0 +1,20 @@
+package org.naga.project.android.action;
+
+import org.naga.project.android.mikuroid.widget.scene.Scene;
+
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+abstract public class Action {
+
+  public Action(Scene s) {
+    this.scene = s;
+  }
+
+  public abstract int update(Intent intent);
+
+  public abstract void view(RemoteViews views);
+
+  protected Scene scene;
+
+}
