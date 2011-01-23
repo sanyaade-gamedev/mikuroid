b789dc2beca4c829add0c4df098f21e392a20e72
reciente <reciente@localhost>
2011-01-24 05:37:59 +0900

    Change scene designed,  abstract class to interface.

--- src/org/naga/project/android/mikuroid/widget/scene/Scene.java
+++ src/org/naga/project/android/mikuroid/widget/scene/Scene.java
@@ -1,36 +1,13 @@
 package org.naga.project.android.mikuroid.widget.scene;
 
-import org.naga.project.android.message.MessageTalk;
-
 import android.content.Intent;
 
-public abstract class Scene {
-
-  public Scene(Scene sc) {
-    super();
-
-    this.finish = false;
-    this.scene = sc;
-  }
+public interface Scene {
 
   public abstract boolean create();
 
   public abstract void onUpdate(Intent intent);
 
   public abstract void onView();
-
-  private Scene scene;
-
-  protected boolean finish;
-
-  public Scene getScene() {
-    return scene;
-  }
-
-  public void setScene(Scene scene) {
-    this.scene = scene;
-  }
-
-  public abstract MessageTalk getMessageTalk();
 
 }
