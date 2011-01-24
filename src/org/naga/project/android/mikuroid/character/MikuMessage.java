527f4489ee3a0a280f884a02d5dcfa96f3d0a598
reciente <reciente@localhost>
2011-01-25 04:26:02 +0900

    Add battery charging message.

--- src/org/naga/project/android/mikuroid/character/MikuMessage.java
+++ src/org/naga/project/android/mikuroid/character/MikuMessage.java
@@ -3,39 +3,74 @@ package org.naga.project.android.mikuroid.character;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.naga.project.android.Information;
 import org.naga.project.android.mikuroid.R;
 import org.naga.project.android.mikuroid.widget.WidgetManager;
+
+import android.os.BatteryManager;
 
 public abstract class MikuMessage {
 
   /**
    * Battery is low when battery level is lower than this number.
    */
-  private static final int BATTERY_LOW_LEVEL = 30;
+  private static final int BATTERY_LOW_LEVEL = 50;
+
+  /**
+   * Battery is very low when battery level is lower than this number.
+   */
+  private static final int BATTERY_DANGER_LEVEL = 20;
+
+  /**
+   * Battery is deadly when battery level is lower than this number.
+   */
+  private static final int BATTERY_DEADLY_LEVEL = 5;
 
   /**
    * Battery is FULL when battery level is equal this number.
    */
   private static final int BATTERY_FULL_LEVEL = 100;
 
+  /**
+   * Generate messages about battery.
+   * 
+   * @return
+   */
   public static List<String> generateBatteryLevelMessage() {
-    Integer batteryLevel = WidgetManager.getInstance().information
-        .getBatteryLevel();
-
     List<String> messageList = new ArrayList<String>();
 
-    if (batteryLevel.intValue() < MikuMessage.BATTERY_LOW_LEVEL) {
-      messageList.add(String.format(WidgetManager.getInstance().getContext()
-          .getResources().getString(R.string.battery_low),
-          batteryLevel.intValue()));
-    } else if (batteryLevel.intValue() == MikuMessage.BATTERY_FULL_LEVEL) {
-      messageList.add(String.format(WidgetManager.getInstance().getContext()
-          .getResources().getString(R.string.battery_full),
-          batteryLevel.intValue()));
+    Information info = WidgetManager.getInstance().information;
+
+    String statusMsg = null;
+    if (info.getBatteryStatus().intValue() == BatteryManager.BATTERY_STATUS_CHARGING) {
+      statusMsg = WidgetManager.getInstance().getContext().getResources()
+          .getString(R.string.battery_charging);
+    }
+
+    int batteryLevel = info.getBatteryLevel().intValue();
+
+    String levelMsg = null;
+    if (batteryLevel < MikuMessage.BATTERY_DEADLY_LEVEL) {
+      levelMsg = String.format(WidgetManager.getInstance().getContext()
+          .getResources().getString(R.string.battery_deadly), batteryLevel);
+    } else if (batteryLevel < MikuMessage.BATTERY_DANGER_LEVEL) {
+      levelMsg = String.format(WidgetManager.getInstance().getContext()
+          .getResources().getString(R.string.battery_danger), batteryLevel);
+    } else if (batteryLevel < MikuMessage.BATTERY_LOW_LEVEL) {
+      levelMsg = String.format(WidgetManager.getInstance().getContext()
+          .getResources().getString(R.string.battery_low), batteryLevel);
+    } else if (batteryLevel == MikuMessage.BATTERY_FULL_LEVEL) {
+      levelMsg = String.format(WidgetManager.getInstance().getContext()
+          .getResources().getString(R.string.battery_full), batteryLevel);
     } else {
-      messageList.add(String.format(WidgetManager.getInstance().getContext()
-          .getResources().getString(R.string.battery_good),
-          batteryLevel.intValue()));
+      levelMsg = String.format(WidgetManager.getInstance().getContext()
+          .getResources().getString(R.string.battery_good), batteryLevel);
+    }
+
+    if (null != statusMsg) {
+      messageList.add(statusMsg + levelMsg);
+    } else {
+      messageList.add(levelMsg);
     }
 
     return messageList;
