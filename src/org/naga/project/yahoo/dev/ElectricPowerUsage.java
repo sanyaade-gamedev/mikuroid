24aa3fb720c2fd19adaf9fcafad7dd7b4b82c6a2
reciente <reciente@localhost>
2011-07-02 15:50:30 +0900

    Add class to manage electronic power usage.

---
+++ src/org/naga/project/yahoo/dev/ElectricPowerUsage.java
@@ -0,0 +1,17 @@
+package org.naga.project.yahoo.dev;
+
+import java.util.Date;
+
+public class ElectricPowerUsage {
+  
+  public String area;
+  
+  public int usage;
+  
+  public int capacity;
+  
+  public Date date;
+  
+  public int hour;
+
+}
---
+++ src/org/naga/project/yahoo/dev/Network.java
@@ -0,0 +1,11 @@
+package org.naga.project.yahoo.dev;
+
+public class Network {
+
+  public static final String API = "PFmZutyxg67OQHwCvFrgTRWu5jpcxcjRinDc.HNBPf.El13gdfWLKImGq6TOLgbY1AI-";
+  
+  public static final String tokyo = "tokyo";
+  public static final String tohoku = "tohoku";
+  public static final String kansai = "kansai";
+
+}
