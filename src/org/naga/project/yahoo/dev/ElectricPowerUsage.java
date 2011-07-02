7b78f70dd8138071045bdf9c5db0b6bda9441fe5
reciente <reciente@localhost>
2011-07-02 16:14:08 +0900

    Create ElectricUsage request class.

--- src/org/naga/project/yahoo/dev/ElectricPowerUsage.java
+++ src/org/naga/project/yahoo/dev/ElectricPowerUsage.java
@@ -1,17 +1,80 @@
 package org.naga.project.yahoo.dev;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class ElectricPowerUsage {
-  
-  public String area;
-  
-  public int usage;
-  
-  public int capacity;
-  
-  public Date date;
-  
-  public int hour;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
+import android.util.Xml;
+
+/**
+ * Entity class for Yahoo Developmentwork ElectricPowerUsage
+ * 
+ * @author reciente
+ * 
+ */
+public class ElectricPowerUsage {
+
+  private static final String DATE_FORMAT = "yyyy-MM-dd";
+
+  public String area;
+
+  public Integer usage;
+
+  public Integer capacity;
+
+  public Date date;
+
+  public Integer hour;
+
+  public void Load(InputStream is) {
+    XmlPullParser parser = Xml.newPullParser();
+    try {
+      parser.setInput(is, "UTF-8");
+    } catch (XmlPullParserException e) {
+      e.printStackTrace();
+      return;
+    }
+
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+        ElectricPowerUsage.DATE_FORMAT);
+
+    try {
+      int eventType = parser.getEventType();
+
+      while (eventType != XmlPullParser.END_DOCUMENT) {
+        String name = null;
+
+        if (eventType == XmlPullParser.START_TAG) {
+          // New entry. <ElectricPowerUsage>
+          name = parser.getName();
+
+          if ("Area".equals(name)) {
+            this.area = parser.nextText();
+          } else if ("Usage".equals(name)) {
+            this.usage = new Integer(parser.nextText());
+          } else if ("Capacity".equals(name)) {
+            this.capacity = new Integer(parser.nextText());
+          } else if ("Date".equals(name)) {
+            try {
+              this.date = simpleDateFormat.parse(parser.nextText());
+            } catch (ParseException e) {
+              e.printStackTrace();
+              this.date = null;
+            }
+          } else if ("Hour".equals(name)) {
+            this.hour = new Integer(parser.nextText());
+          }
+        }
+      }
+    } catch (XmlPullParserException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
--- src/org/naga/project/yahoo/dev/Network.java
+++ src/org/naga/project/yahoo/dev/Network.java
@@ -1,11 +1,74 @@
 package org.naga.project.yahoo.dev;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.util.Log;
+
 public class Network {
 
+  private static final String API_URL = "http://setsuden.yahooapis.jp/v1/Setsuden/latestPowerUsage";
+
   public static final String API = "PFmZutyxg67OQHwCvFrgTRWu5jpcxcjRinDc.HNBPf.El13gdfWLKImGq6TOLgbY1AI-";
-  
+
   public static final String tokyo = "tokyo";
   public static final String tohoku = "tohoku";
   public static final String kansai = "kansai";
 
+  public ElectricPowerUsage requestElectricPowerUsage() {
+    String request_url = Network.API_URL + "?appid=" + Network.API;
+    // TODO Set request parameters
+
+    final HttpClient httpClient = new DefaultHttpClient();
+
+    HttpGet httpGet = new HttpGet(request_url);
+
+    HttpResponse httpResponse = null;
+    try {
+      // HTTP request
+      httpResponse = httpClient.execute(httpGet);
+    } catch (ClientProtocolException e) {
+      e.printStackTrace();
+      httpResponse = null;
+    } catch (IOException e) {
+      e.printStackTrace();
+      httpResponse = null;
+    }
+
+    // Has error?
+    if (null == httpResponse) {
+      return null;
+    }
+
+    Log.d("ElectricPowerUsage HTTP Request", "HTTP STATUS : "
+        + httpResponse.getStatusLine().getStatusCode());
+
+    // Check HTTP response status.
+    if (HttpStatus.SC_OK != httpResponse.getStatusLine().getStatusCode()) {
+      return null;
+    }
+
+    InputStream is = null;
+    ElectricPowerUsage electricPowerUsage = new ElectricPowerUsage();
+    try {
+      is = httpResponse.getEntity().getContent();
+    } catch (IllegalStateException e) {
+      e.printStackTrace();
+      return null;
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    }
+
+    electricPowerUsage.Load(is);
+
+    return electricPowerUsage;
+  }
 }
