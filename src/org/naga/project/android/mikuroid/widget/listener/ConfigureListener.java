4b482ce1951553650a5588e0b3cdfa03bd0893b4
reciente <reciente@localhost>
2011-07-03 02:28:27 +0900

    Add nullpointer check.

--- src/org/naga/project/android/mikuroid/widget/listener/ConfigureListener.java
+++ src/org/naga/project/android/mikuroid/widget/listener/ConfigureListener.java
@@ -9,16 +9,18 @@ import android.content.Intent;
 public class ConfigureListener implements Listener {
 
   public boolean onListen(Intent intent) {
-    if (MikuroidIntent.ACTION_CONFIGURE.equals(intent.getAction())) {
-      // Start configure activity.
-      Intent activityIntent = new Intent(WidgetManager.getInstance()
-          .getContext().getApplicationContext(),
-          MikuroidConfigureActivity.class);
-      activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    if (null != intent && null != intent.getAction()) {
+      if (MikuroidIntent.ACTION_CONFIGURE.equals(intent.getAction())) {
+        // Start configure activity.
+        Intent activityIntent = new Intent(WidgetManager.getInstance()
+            .getContext().getApplicationContext(),
+            MikuroidConfigureActivity.class);
+        activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-      WidgetManager.getInstance().getContext().startActivity(activityIntent);
-      
-      return false;
+        WidgetManager.getInstance().getContext().startActivity(activityIntent);
+
+        return false;
+      }
     }
 
     return true;
